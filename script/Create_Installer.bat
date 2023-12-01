f0d949a5e1fd8bf5e9026ff612eb8b3d3cab1bdc
Andrew J.Swan <ajs.blackhole@gmail.com>
2023-12-01 12:12:00 +0200

    Move MePo Theme to own repository

---
+++ script/Create_Installer.bat
@@ -0,0 +1,27 @@
+@ECHO OFF
+
+Title Creating MediaPortal MePo Theme Installer
+
+ECHO Creating MediaPortal MePo Theme Installer
+
+if "%programfiles(x86)%XXX"=="XXX" goto 32BIT
+    :: 64-bit
+    set PROGS=%programfiles(x86)%
+    goto CONT
+:32BIT
+    set PROGS=%ProgramFiles%
+:CONT
+
+IF NOT EXIST "%PROGS%\Team MediaPortal\MediaPortal\" SET PROGS=C:
+
+:: Get version from DLL
+FOR /F "tokens=*" %%a IN ('git rev-list HEAD --count') DO SET version=%%a
+
+:: Trim version
+SET version=%version: =%
+
+:: Set version
+SET version=5.0.0.%version%
+
+:: Build MPE1
+"%PROGS%\Team MediaPortal\MediaPortal\MPEMaker.exe" "MePoTheme.xmp2" /B /V=%version% /UpdateXML
